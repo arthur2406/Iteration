ed5446bdfd58c6b3a769520fc650ea0c3f594ae2
arthur2406 <rizhiy2406@gmail.com>
2019-10-25 20:26:54 +0300

    labs 1-7

--- Exercises/2-for-of.js
+++ Exercises/2-for-of.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const sum = (...args) => {
-  // Use for..of loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  let value = 0;
+  for (const num of args) {
+    value += num;
+  }
+  return value;
 };
 
 module.exports = { sum };
